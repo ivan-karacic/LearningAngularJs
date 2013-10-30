27915b4f0f45310c23c2a43416b64bb27e58bdfb
ivan.karacic <ivan.karacic@qualtrak.com>
2013-10-30 11:14:34 +0100

    Added  - TenantTreeview.coffee  - treeview.json data
    Added service to tenant controller

--- static/app/tenant/TenantService.js
+++ static/app/tenant/TenantService.js
@@ -1,13 +1,23 @@
 ﻿(function() {
   var administratorService;
 
-
-
   administratorService = angular.module('administratorService', ['ngResource']);
 
   administratorService.factory('Tenant', [
     '$resource', function($resource) {
       return $resource('../assets/data/tenant.json', {}, {
+        query: {
+          method: 'GET',
+          params: {},
+          isArray: true
+        }
+      });
+    }
+  ]);
+
+  administratorService.factory('Treeview', [
+    '$resource', function($resource) {
+      return $resource('../assets/data/treeview.json', {}, {
         query: {
           method: 'GET',
           params: {},
